ab7cfb9c6a1c5b99301ea4c4599c69ab0cf86af7
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-03 12:50:32 +0530

    Update Soundex.cs

--- Soundex.cs
+++ Soundex.cs
@@ -1,18 +1,12 @@
 using System;
-using System.Collections.Generic;
 using System.Text;
 
 public class Soundex
 {
-    private static readonly Dictionary<char, char> SoundexMapping = new Dictionary<char, char>
-    {
-        { 'B', '1' }, { 'F', '1' }, { 'P', '1' }, { 'V', '1' },
-        { 'C', '2' }, { 'G', '2' }, { 'J', '2' }, { 'K', '2' }, { 'Q', '2' },
-        { 'S', '2' }, { 'X', '2' }, { 'Z', '2' },
-        { 'D', '3' }, { 'T', '3' },
-        { 'L', '4' },
-        { 'M', '5' }, { 'N', '5' },
-        { 'R', '6' }
+    private static readonly string[] SoundexMapping = {
+        // A, B, C, D, E, F, G, H, I, J, K, L, M
+        "01230120022455012623010202", // N, O, P, Q, R, S, T, U, V, W, X, Y, Z
+        "00000000000000000000000000"
     };
 
     public static string GenerateSoundex(string name)
@@ -45,6 +39,10 @@ public class Soundex
     private static char GetSoundexCode(char c)
     {
         c = char.ToUpper(c);
-        return SoundexMapping.TryGetValue(c, out char code) ? code : '0';
+        if (c < 'A' || c > 'Z')
+            return '0';
+
+        int index = c - 'A';
+        return SoundexMapping[0][index];
     }
 }
