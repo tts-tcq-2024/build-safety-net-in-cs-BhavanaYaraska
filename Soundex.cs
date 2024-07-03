58d3c00a15ec02137481ff8c4c87f5ecddf30a8b
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-03 13:02:30 +0530

    Update Soundex.cs

--- Soundex.cs
+++ Soundex.cs
@@ -3,50 +3,53 @@ using System.Text;
 
 public class Soundex
 {
-    private static readonly string[] SoundexMapping = {
-        "01230120022455012623010202", // A-M
-        "00000000000000000000000000"  // N-Z
-    };
+  private static readonly string[] SoundexMapping = {
+    "01230120022455012623010202", // A-M
+    "00000000000000000000000000"  // N-Z
+  };
 
-    public static string GenerateSoundex(string name)
+  public static string GenerateSoundex(string name)
+  {
+    if (string.IsNullOrEmpty(name))
     {
-        if (string.IsNullOrEmpty(name))
-            return string.Empty;
-
-        StringBuilder soundex = new StringBuilder();
-        soundex.Append(char.ToUpper(name[0]));
-        char prevCode = GetSoundexCode(name[0]);
-
-        for (int i = 1; i < name.Length && soundex.Length < 4; i++)
-        {
-            char code = GetSoundexCode(name[i]);
-            if (code != '0' && code != prevCode)
-            {
-                soundex.Append(code);
-                prevCode = code;
-            }
-        }
-
-        PadSoundex(soundex);
-
-        return soundex.ToString();
+      return string.Empty;
     }
 
-    private static void PadSoundex(StringBuilder soundex)
+    string firstChar = char.ToUpper(name[0]).ToString();
+    string remainingChars = name.Substring(1);
+
+    string encodedChars = EncodeChars(remainingChars);
+
+    return firstChar + encodedChars.PadRight(4, '0');
+  }
+
+  private static string EncodeChars(string chars)
+  {
+    StringBuilder encoded = new StringBuilder();
+    char prevCode = ' '; // Use a non-matching character for initial comparison
+
+    foreach (char c in chars)
     {
-        while (soundex.Length < 4)
-        {
-            soundex.Append('0');
-        }
+      char code = GetSoundexCode(c);
+      if (code != '0' && code != prevCode)
+      {
+        encoded.Append(code);
+        prevCode = code;
+      }
     }
 
-    private static char GetSoundexCode(char c)
-    {
-        c = char.ToUpper(c);
-        if (c < 'A' || c > 'Z')
-            return '0';
+    return encoded.ToString();
+  }
 
-        int index = c - 'A';
-        return SoundexMapping[index / 13][index % 13];
+  private static char GetSoundexCode(char c)
+  {
+    c = char.ToUpper(c);
+    int index = c - 'A';
+    if (index < 0 || index >= SoundexMapping.Length * SoundexMapping[0].Length)
+    {
+      return '0';
     }
+
+    return SoundexMapping[index / 13][index % 13];
+  }
 }
